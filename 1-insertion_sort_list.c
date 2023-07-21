d1b8f236cf1528ec1c707c8f3897d4bee1ff11e9
salihabrik <briksaliha@gmail.com>
2023-07-22 01:00:33 +0200

    If you need more than 3 levels of indentation, you're screwed anyway, and should fix your program.

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked
+ * @list: Pointer to the head of the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			/* Swap the nodes */
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			/* If temp is now the head, update the list pointer */
+			if (temp->prev == NULL)
+				*list = temp;
+
+			/* Print the list after each swap */
+			print_list(*list);
+		}
+	}
+}
