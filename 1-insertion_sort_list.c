cbda65b2b74f151ea408a28cbbbd961f01b34cdd
salihabrik <briksaliha@gmail.com>
2023-07-23 19:25:08 +0200

    Be humble, communicate clearly, and respect others. It costs nothing to be kind, but the impact is priceless

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,6 +11,7 @@
  */
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current;
@@ -45,27 +46,18 @@ void insertion_sort_list(listint_t **list)
 	}
 =======
  
+=======
+>>>>>>> 3c3d3ce (Be humble, communicate clearly, and respect others. It costs nothing to be kind, but the impact is priceless)
 void insertion_sort_list(listint_t **list)
 {
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	listint_t *current;
 
-    listint_t *current = (*list)->next;
-    while (current != NULL)
-    {
-        listint_t *temp = current;
-        while (temp->prev != NULL && temp->n < temp->prev->n)
-        {
-            if (temp->next != NULL)
-                temp->next->prev = temp->prev;
-            temp->prev->next = temp->next;
+	if (*list == NULL || (*list)->next == NULL)
+		return;
 
-            if (temp->prev->prev != NULL)
-                temp->prev->prev->next = temp;
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
+	current = (*list)->next;
 
+<<<<<<< HEAD
 =======
  
 void insertion_sort_list(listint_t **list)
@@ -103,3 +95,29 @@ void insertion_sort_list(listint_t **list)
 }
 
 >>>>>>> 2ec9759 (Be humble, communicate clearly, and respect others. It costs nothing to be kind, but the impact is priceless)
+=======
+	while (current != NULL)
+	{
+		listint_t *temp = current;
+
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			listint_t *prev = temp->prev;
+			listint_t *next = temp->next;
+
+			if (prev->prev != NULL)
+				prev->prev->next = temp;
+			else
+				*list = temp;
+			if (next != NULL)
+				next->prev = prev;
+			temp->prev = prev->prev;
+			temp->next = prev;
+			prev->prev = temp;
+			prev->next = next;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
+>>>>>>> 3c3d3ce (Be humble, communicate clearly, and respect others. It costs nothing to be kind, but the impact is priceless)
