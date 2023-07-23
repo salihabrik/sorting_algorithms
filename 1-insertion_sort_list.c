2ec975934dbf039db8757d85109870c1cb9a0597
salihabrik <briksaliha@gmail.com>
2023-07-23 19:12:42 +0200

    Be humble, communicate clearly, and respect others. It costs nothing to be kind, but the impact is priceless

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,36 +4,33 @@
  * insertion_sort_list - Sorts a doubly linked
  * @list: Pointer to the head of the doubly linked list
  */
-
+ 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *temp;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    listint_t *current = (*list)->next;
+    while (current != NULL)
+    {
+        listint_t *temp = current;
+        while (temp->prev != NULL && temp->n < temp->prev->n)
+        {
+            if (temp->next != NULL)
+                temp->next->prev = temp->prev;
+            temp->prev->next = temp->next;
 
-	current = (*list)->next;
-	while (current != NULL)
-	{
-		temp = current;
-		current = current->next;
+            if (temp->prev->prev != NULL)
+                temp->prev->prev->next = temp;
+            temp->next = temp->prev;
+            temp->prev = temp->prev->prev;
+            temp->next->prev = temp;
 
-		while (temp->prev != NULL && temp->n < temp->prev->n)
-		{
-			/* Swap the nodes */
-			if (temp->next != NULL)
-				temp->next->prev = temp->prev;
-			temp->prev->next = temp->next;
-			temp->next = temp->prev;
-			temp->prev = temp->prev->prev;
-			temp->next->prev = temp;
-
-			/* If temp is now the head, update the list pointer */
-			if (temp->prev == NULL)
-				*list = temp;
-
-			/* Print the list after each swap */
-			print_list(*list);
-		}
-	}
+            if (temp->prev == NULL)
+                *list = temp;
+            print_list(*list);
+        }
+        current = current->next;
+    }
 }
+
