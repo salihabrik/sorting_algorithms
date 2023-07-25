b9321648b6e1f246405c67eef03cc28344031eb3
salihabrik <briksaliha@gmail.com>
2023-07-25 14:59:18 +0200

    Typing is no substitute for thinking

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,6 +10,7 @@
  * @list: Pointer to the head of the doubly linked list
  */
 <<<<<<< HEAD
+<<<<<<< HEAD
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current;
@@ -65,11 +66,40 @@ void insertion_sort_list(listint_t **list)
             temp->prev = temp->prev->prev;
             temp->next->prev = temp;
 
+=======
+ 
+void insertion_sort_list(listint_t **list)
+{
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+
+    listint_t *current = (*list)->next;
+    while (current != NULL)
+    {
+        listint_t *temp = current;
+        while (temp->prev != NULL && temp->n < temp->prev->n)
+        {
+            if (temp->next != NULL)
+                temp->next->prev = temp->prev;
+            temp->prev->next = temp->next;
+
+            if (temp->prev->prev != NULL)
+                temp->prev->prev->next = temp;
+            temp->next = temp->prev;
+            temp->prev = temp->prev->prev;
+            temp->next->prev = temp;
+
+>>>>>>> 2ec9759 (Be humble, communicate clearly, and respect others. It costs nothing to be kind, but the impact is priceless)
             if (temp->prev == NULL)
                 *list = temp;
             print_list(*list);
         }
         current = current->next;
     }
+<<<<<<< HEAD
 
 }
+=======
+}
+
+>>>>>>> 2ec9759 (Be humble, communicate clearly, and respect others. It costs nothing to be kind, but the impact is priceless)
